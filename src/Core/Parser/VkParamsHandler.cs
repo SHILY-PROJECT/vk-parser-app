b916ef3158ac9819bddac7d95367b507fefa15c4
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-02 22:33:28 +0300

    -Добавлен маппинг свойств моделек

--- src/Core/Parser/VkParamsHandler.cs
+++ src/Core/Parser/VkParamsHandler.cs
@@ -10,11 +10,7 @@ public static class VkParamsHandler
         try
         {
             var rxJsonParams = new Regex(@"(?<=window\.init\ =\ )\{.*?}(?=;)");
-            
-            /*
-             *  TODO: Добавить маппинг
-             */
-
+            Map(vkParams, JsonSerializer.Deserialize<VkParamsDeserializationModel>(rxJsonParams.Match(html).Value));
             return vkParams.IsTokensFound();
         }
         catch (Exception ex)
@@ -22,6 +18,20 @@ public static class VkParamsHandler
             Console.WriteLine(ex.Message);
         }
         return false;
+    }
+
+    private static void Map(VkParams? vkParams, VkParamsDeserializationModel? vkParamsDeserialization)
+    {
+        if (vkParams is null) throw new ArgumentNullException(nameof(vkParams));
+        if (vkParamsDeserialization is null) throw new ArgumentNullException(nameof(vkParamsDeserialization));
+
+        foreach (var prop in typeof(VkParamsDeserializationModel).GetProperties())
+        {
+            if (prop.GetValue(vkParamsDeserialization) is object value && (value.GetType() != typeof(string) || !string.IsNullOrWhiteSpace(value as string)))
+            {
+                typeof(VkParams)?.GetProperty(prop.Name)?.SetValue(vkParams, value);
+            }
+        }
     }
 
     private static bool IsTokensFound(this VkParams vkParams) =>
