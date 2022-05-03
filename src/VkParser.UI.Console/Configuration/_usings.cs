58988726a73aaf74a372a46414f1f2a1bc5750fc
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-03 17:01:54 +0300

    -Добавлен отдельно проект под UI консоли

---
+++ src/VkParser.UI.Console/Configuration/_usings.cs
@@ -0,0 +1,13 @@
+﻿global using System;
+global using System.Net;
+global using System.Net.Http.Headers;
+global using System.Web;
+global using System.Text;
+global using System.Text.Json;
+global using System.Text.Json.Nodes;
+global using System.Text.Json.Serialization;
+global using System.Text.RegularExpressions;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using System.Collections.Generic;
+global using Microsoft.Extensions.DependencyInjection;
---
+++ src/VkParser.UI.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+//var client = new VkFacade();
+
+//var loginPass = File.ReadAllLines(@"F:\SHILY PROJECT\Projects\vk\sec.txt", Encoding.UTF8);
+
+//var acc = new UserDataModel
+//{
+//    Login = loginPass[0],
+//    Password = loginPass[1]
+//};
+
+//var response = await client.Auth(acc);
+
+//Console.ReadLine();
