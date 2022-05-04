1d7754633bc45d67d77219c5db31f3a7ba4028dd
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-04 23:16:45 +0300

    -Добавлен парсер

---
+++ src/VkParser.WebRequests/Parser/VkParserWebRequests.cs
@@ -0,0 +1,19 @@
+﻿namespace VkParser.WebRequests.Parser;
+
+public class VkParserWebRequests : IParser
+{
+    private HttpClient _client;
+
+    public VkParserWebRequests(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<IEnumerable<IUser>> ParseUsersAsync(IParsingOptions options)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, options.Url);
+        var response = await _client.SendAsync(request);
+
+        throw new NotImplementedException();
+    }
+}
