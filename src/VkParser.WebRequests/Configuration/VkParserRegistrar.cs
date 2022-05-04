fb8c37bb7d5af501b4b71d86443a24db7cfc7fc1
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-04 23:35:27 +0300

    -Изменена конфигурация для http клиента -Какие-то ещё фиксы

---
+++ src/VkParser.WebRequests/Configuration/VkParserRegistrar.cs
@@ -0,0 +1,26 @@
+﻿namespace VkParser.WebRequests.Configuration;
+
+public static class VkParserRegistrar
+{
+    public static IServiceCollection AddVkParserComponents(this IServiceCollection services)
+    {
+        var cfg = HttpClientConfiguration.CreateConfiguration();
+
+        services
+            .AddScoped<VkParams>(opt => VkParams.CreateAnObjectWithDefaultParams())
+            .AddScoped<IHttpClientConfiguration>(opt => cfg)
+            .AddTransient<HttpClient>(opt => ConfigureHttpClient(cfg))
+            .AddTransient<IVkFacade, WebRequestsFacade>()
+            .AddTransient<IAuthorizer, VkAuthorizer>()
+            .AddTransient<IParser, VkParserWebRequests>();
+
+        return services;
+    }
+
+    private static HttpClient ConfigureHttpClient(IHttpClientConfiguration cfg)
+    {
+        var client = new HttpClient(cfg.HttpClientHandler);
+        client.DefaultRequestHeaders.AddRange(cfg.DefaultHeaders);
+        return client;
+    }
+}
